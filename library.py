a49d06617fccbd1e63552eaef05dbb3a31dcaf6e
stamanna <99626568+stamanna@users.noreply.github.com>
2023-10-15 14:22:49 -0700

    Create library.py

---
+++ library.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import sklearn
+sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
