f23e379a25379d52dd67358ecfaa307c1457801e
stamanna <99626568+stamanna@users.noreply.github.com>
2023-11-06 09:51:16 -0800

    Update library.py

--- library.py
+++ library.py
@@ -270,3 +270,24 @@ customer_transformer = Pipeline(steps=[
     ('scale_time spent', CustomRobustTransformer('Time Spent')), #from 5
     ('impute', KNNImputer(n_neighbors=5, weights="uniform", add_indicator=False)),
     ], verbose=True)
+
+def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts=.2):
+  #your code below
+  labels = original_table[label_column_name].to_list()
+  feat = original_table.drop(columns = label_column_name)
+  X_train, X_test, y_train, y_test = train_test_split(feat, labels, test_size=ts, shuffle=True,
+                                                    random_state=rs, stratify=labels)
+  
+  X_train_transformed = the_transformer.fit_transform(X_train, y_train)
+  X_test_transformed = the_transformer.transform(X_test)
+
+  X_train_numpy = X_train_transformed.to_numpy()
+  X_test_numpy = X_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test) 
+  
+def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
+  return dataset_setup(customer_table, 'Rating', transformer, rs=rs, ts=ts)
+  
+def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
+  return dataset_setup(customer_table, 'Rating', transformer, rs=rs, ts=ts)
