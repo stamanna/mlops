eec305615a5d1ab08583acaaf5a2f1fcc18d7ee0
stamanna <99626568+stamanna@users.noreply.github.com>
2023-11-12 00:55:56 -0800

    Update library.py

--- library.py
+++ library.py
@@ -288,7 +288,7 @@ def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts
   return X_train_numpy, X_test_numpy, y_train_numpy, y_test_numpy
   
 def titanic_setup(titanic_table, transformer=titanic_transformer, rs=titanic_variance_based_split, ts=.2):
-  return dataset_setup(titanic_table, 'Survived', transformer, rs=rs, ts=ts)
+  return dataset_setup(titanic_table, 'Survived', transformer, rs=rs, ts=ts) 
   
 def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
   return dataset_setup(customer_table, 'Rating', transformer, rs=rs, ts=ts)
